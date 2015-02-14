0e41a47fa58a415209a4c886944e3a8768c779e7
agauniyal <agauniyal@outlook.com>
2015-02-14 12:30:10 +0530

    Added countdown timer

---
+++ js/countdown.js
@@ -0,0 +1,34 @@
+function CountDownTimer(dt, id) {
+    var end = new Date(dt);
+
+    var _second = 1000;
+    var _minute = _second * 60;
+    var _hour = _minute * 60;
+    var _day = _hour * 24;
+    var timer;
+
+    function showRemaining() {
+        var now = new Date();
+        var distance = end - now;
+        if (distance < 0) {
+
+            clearInterval(timer);
+            document.getElementById(id).innerHTML = 'Started!';
+
+            return;
+        }
+        var days = Math.floor(distance / _day);
+        var hours = Math.floor((distance % _day) / _hour);
+        var minutes = Math.floor((distance % _hour) / _minute);
+        var seconds = Math.floor((distance % _minute) / _second);
+
+        document.getElementById(id).innerHTML = days + 'days ';
+        document.getElementById(id).innerHTML += hours + 'hrs ';
+        document.getElementById(id).innerHTML += minutes + 'mins ';
+        document.getElementById(id).innerHTML += seconds + 'secs';
+    }
+
+    timer = setInterval(showRemaining, 1000);
+}
+
+CountDownTimer('02/26/2015 10:1 AM', 'countdown');
